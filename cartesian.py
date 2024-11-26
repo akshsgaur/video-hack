a3d998a9d1b5826e643cd019e0d52e57f3da8ac9
Akshit Gaur <akshitgaur@Akshits-MacBook-Pro.local>
2024-11-25 22:50:43 -0800

    first commit

---
+++ cartesian.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+from cartesia import Cartesia
+import simpleaudio as sa
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Access the keys
+api_key = os.getenv('CARTESIA_API_KEY')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(api_key)
+client = Cartesia(api_key=api_key)
+
+
+data = client.tts.bytes(
+    model_id="sonic-english",
+    transcript="""Create a new project with Cursor, Replit or your favorite tool
+
+​Design a character with FLUX or your favorite image model
+
+​Add a personality with the world's fastest LLMs
+
+​Connect a custom voice with Cartesia's 95ms Sonic TTS API
+
+​Bring your avatar to life with Simli's 300ms Avatar API""",
+    voice_id="a0e99841-438c-4a64-b679-ae501e7d6091",  # Barbershop Man
+    # You can find the supported `output_format`s at https://docs.cartesia.ai/api-reference/tts/bytes
+    output_format={
+        "container": "wav",
+        "encoding": "pcm_f32le",
+        "sample_rate": 44100,
+    },
+)
+
+#print(data)
+with open("sonic.wav", "wb") as f:
+    f.write(data)
+
+
+wave_obj = sa.WaveObject.from_wave_file(f"{current_dir}/sonic.wav")
+play_obj = wave_obj.play()
+play_obj.wait_done()
